376774e42b296d0ab840a22df6f49710c03695a2
QuangHung <duongquanghung0122@gmail.com>
2022-06-16 11:28:14 +0700

    Components Part 3_2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
 import MenuComponent from "./components/MenuComponent";
+import { DISHES } from "./shared/dish";
+import Menu from "./components/MenuComponent";
+import { Navbar, NavbarBrand } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <MenuComponent />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />;
+      </div>
+    );
+  }
 }
 export default App;
